06806c433f68d4c38ce790421eb741e73d9cc367
Ben Hong <bencodezen@gmail.com>
2019-02-25 18:39:45 -0500

    feature (#1): Add ability to add rectangles

---
+++ src/components/SvgCanvas.vue
@@ -0,0 +1,22 @@
+<script>
+export default {
+  render(createElement) {
+    return createElement(
+      'svg',
+      {
+        viewBox: "0 0 100px 100px"
+      },
+      this.elements.map(element => 
+        createElement(element.tag, {
+          attrs: element.attrs
+        })
+      )
+    )
+  },
+  props: {
+    elements: {
+      type: Array
+    }
+  }
+}
+</script>
---
+++ src/views/Generator.vue
@@ -0,0 +1,45 @@
+<script>
+import SvgCanvas from '../components/SvgCanvas'
+
+export default {
+  components: {
+    SvgCanvas
+  },
+  data() {
+    return {
+      elements: [],
+      rectWidth: 0
+    }
+  },
+  methods: {
+    addRectangle() {
+      this.elements.push(
+        {
+          tag: 'rect',
+          attrs: {
+            width: 50,
+            height: 50,
+            x: 10,
+            y: 20
+          }
+        }
+      )
+    }
+  }
+}
+</script>
+
+<template>
+	<div>
+    <svg-canvas :elements="elements"></svg-canvas>
+    <div class="palette">
+      <button @click="addRectangle">Add Rectangle</button>
+    </div>
+  </div>
+</template>
+
+<style lang="scss">
+svg {
+  border: 1px solid red;
+}
+</style>
