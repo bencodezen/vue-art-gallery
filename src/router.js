8ec480876965e25be9a412473eec72a0da55e76b
Ben Hong <bencodezen@gmail.com>
2019-02-25 18:39:12 -0500

    feature: Scaffold generator page

--- src/router.js
+++ src/router.js
@@ -20,6 +20,12 @@ export default new Router({
       // which is lazy-loaded when the route is visited.
       component: () =>
         import(/* webpackChunkName: "about" */ "./views/About.vue")
+    },
+    {
+      path: "/generator",
+      name: "generator",
+      component: () => 
+        import(/* webpackChunkName: "generator" */ "./views/Generator.vue")
     }
   ]
 });
